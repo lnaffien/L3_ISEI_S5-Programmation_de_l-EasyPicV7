d5b5e30b3f7aea85c9f26b71cdd0b7da183a21d7
lucie <arthur.laurain@outloo.fr>
2021-11-22 12:04:43 +0100

    Afficheur 7 segments 1 digit - non teste

--- assembleur/TP2-Clignoter_LEDs/main.asm
+++ assembleur/TP2-Clignoter_LEDs/main.asm
@@ -15,7 +15,7 @@ main:
       BSF      STATUS,5
       BCF      STATUS,6
       BCF	   TRISE, 2
-      BCF		STATUS,5
+      BCF	      STATUS,5
       BCF 		STATUS,6
       BSF      PORTE,2
       CALL      tempo
@@ -24,9 +24,12 @@ main:
       GOTO      main
 
 tempo:
-      MOVLW      B'11111001';valeur max qu'on peut utiliser pour la temporisation: B'00100001'
+      ;Move Litteral to W
+      MOVLW      B'11111001';valeur max qu'on peut utiliser pour la temporisation: B'00100001' 
+      ;Move W to F
       MOVWF      V1
 Do:   MOVWF      V2
+        ;Decrement F, Skip if 0
 	  DECFSZ      V2,1
       goto      $-1
 	  DECFSZ      V1,1 ;Le petit d est un switch, si on met d=0   on stoque dans W, sinon dans le file register
--- assembleur/TP3.1-7_segment/main.asm
+++ assembleur/TP3.1-7_segment/main.asm
@@ -1,6 +1,128 @@
 #include <PIGF877A.inc>
-__CONFIG _HS_OSC & _WOT_OFF & _LVP_OFF        
+__CONFIG _HS_OSC & _WOT_OFF & _LVP_OFF      
+CBLOCK 0x20 ;Reserver une zone de variable 
+    Valeur_tempo : 2
+    ENDC
 org 0x0000
 goto main
 
 main :
+    ;Initialisation
+    BSF STATUS, RP0 ;Aller a la page memoire 1
+    BCF STATUS, RP1
+    BCF TRISA, 0    ;PORTA PIN0 = sortie
+    BCF TRISD, 0    ;PORTD = sortie
+    BCF TRISD, 1
+    BCF TRISD, 2
+    BCF TRISD, 3
+    BCF TRISD, 4
+    BCF TRISD, 5
+    BCF TRISD, 6
+    BCF TRISD, 7
+    BCF STATUS, RP0 ;Retour a la page memoire 0
+    GOTO afficher1 ;Test de l'initialisation et de la syntaxe pour l'affichage
+boucle :
+    CALL afficher0
+    CALL tempo
+    CALL afficher1
+    CALL tempo
+    CALL afficher2
+    CALL tempo
+    CALL afficher3
+    CALL tempo
+    CALL afficher4
+    CALL tempo
+    CALL afficher5
+    CALL tempo
+    CALL afficher6
+    CALL tempo
+    CALL afficher7
+    CALL tempo
+    CALL afficher8
+    CALL tempo
+    CALL afficher9
+    CALL tempo
+    GOTO boucle
+tempo :
+    MOVLW B'0011100100000000' ;Valeur de la tempo
+    MOVWF Valeur_tempo
+Do :
+    DECSFZ Valeur_tempo, 1
+    goto $-1
+    RETURN
+clean :
+    BCF PORTD
+afficher0 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 4
+    BSF PORTD, 5
+    BSF PORTD, 6
+afficher1 : 
+    CALL clean
+    BSF PORTD, 1
+    BSF PORTD, 2
+afficher2 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 3
+    BSF PORTD, 4
+    BSF PORTD, 6
+afficher3 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 6
+afficher4 :
+    CALL clean
+    BCF PORTD
+    BSF PORTD, 1
+    BSF PORTD, 2
+    BSF PORTD, 5
+    BSF PORTD, 6
+afficher5 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 5
+    BSF PORTD, 6
+afficher6 : 
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 4
+    BSF PORTD, 5
+    BSF PORTD, 6
+afficher7 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 2
+afficher8 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 4
+    BSF PORTD, 5
+    BSF PORTD, 6
+    BSF PORTD, 7
+afficher9 :
+    CALL clean
+    BSF PORTD, 0
+    BSF PORTD, 1
+    BSF PORTD, 2
+    BSF PORTD, 3
+    BSF PORTD, 5
+    BSF PORTD, 6
+
+END
